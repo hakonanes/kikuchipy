9ce481dc4235d271a51446ad1ea96cae398d51e9
Håkon Wiik Ånes <hwaanes@gmail.com>
2026-06-13 15:52:14 +0200

    Add reader for master patterns from ebsdsim
    Signed-off-by: Håkon Wiik Ånes <hwaanes@gmail.com>

---
+++ src/kikuchipy/io/plugins/ebsdsim_master_pattern/__init__.py
@@ -0,0 +1,11 @@
+#
+# Copyright 2026 the kikuchipy developers
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+
+import lazy_loader
+
+__getattr__, __dir__, __all__ = lazy_loader.attach_stub(__name__, __file__)
+
+del lazy_loader
